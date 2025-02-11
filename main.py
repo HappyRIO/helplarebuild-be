490ef34220976c03978fc525cd7a026389ef5b72
happyrio <happyrio1013@gmail.com>
2025-02-11 09:18:08 +0900

    embed frontend

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pymilvus import MilvusClient
 
 from search_engine import SearchEngine
-# from indexer import Indexer
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 
 load_dotenv()
 
@@ -16,9 +17,7 @@ milvus_client = MilvusClient(
 )
 milvus_collection_name = os.getenv("MILVUS_COLLECTION_NAME")
 
-# indexer = Indexer(milvus_client, milvus_collection_name)
 searchEngine = SearchEngine(milvus_client, milvus_collection_name)
-print("running-----")
 
 app = FastAPI()
 
@@ -32,16 +31,9 @@ app.add_middleware(
 class Msg(BaseModel):
     msg: str
 
-@app.get("/")
-async def root():
-    return {"message": "/search"}
-
-@app.post("/search")
+@app.post("/api/search")
 async def search(inp: Msg):
     result = searchEngine.search(inp.msg)
     return result
 
-# @app.post("/create_index")
-# async def create_index(inp: Msg):
-#     result = indexer.index_website(inp.msg)
-#     return { "message": "Indexing complete" }
+app.mount("/", StaticFiles(directory="static",html = True), name="static")
